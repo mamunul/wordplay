da2983dcab611c8da10c4ddccc2abaa748345ba0
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-21 18:08:32 +0600

    add game basic gameplay methods in presenter

--- BabbelChallenge/GamePlay/GamePlayPresenter.swift
+++ BabbelChallenge/GamePlay/GamePlayPresenter.swift
@@ -11,6 +11,8 @@ protocol IGamePlayPresenter: ObservableObject {
     var word: String { get set }
     var translation: String { get set }
     var accuracy: String { get set }
+    var showTranslation: Bool { get set }
+    func onAnimationCompletion()
     func onTranslationButtonTapped()
 }
 
@@ -18,13 +20,20 @@ class GamePlayPresenter: IGamePlayPresenter {
     @Published var word: String
     @Published var translation: String
     @Published var accuracy: String
+    @Published var showTranslation: Bool
 
     private var translationMap = [String: String]()
+    private let numberOfOptionsPerQuery = 4
+    private let numberOfQuery = 10
+    private var currentTranslationNo = 0
+    private var correctCount = 0
+    private var totalPresented = 0
 
     init() {
         accuracy = "0/0"
         word = "test"
         translation = "trans-test"
+        showTranslation = false
     }
 
     private func loadTranslationMap() {
@@ -34,6 +43,38 @@ class GamePlayPresenter: IGamePlayPresenter {
     }
 
     func onTranslationButtonTapped() {
-        accuracy = "1/1"
+        accuracy = "\(correctCount)/\(totalPresented)"
+
+        totalPresented += 1
+        currentTranslationNo = 0
+        correctCount += 1
+        checkResult()
+        showNewQuery()
+        if isGameCompleted() {
+            showGameCompletionStatus()
+        }
+    }
+
+    func onAnimationCompletion() {
+        currentTranslationNo += 1
+
+        checkResult()
+        showNewQuery()
+        if isGameCompleted() {
+            showGameCompletionStatus()
+        }
+    }
+
+    private func showNewQuery() {
+    }
+
+    private func isGameCompleted() -> Bool {
+        true
+    }
+
+    private func showGameCompletionStatus() {
+    }
+
+    private func checkResult() {
     }
 }
