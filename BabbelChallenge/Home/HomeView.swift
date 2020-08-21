f7a567c0a738edfe2d15f12c5ca9fd992c7bfae9
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-21 15:14:49 +0600

    add home view with start button

--- BabbelChallenge/Home/HomeView.swift
+++ BabbelChallenge/Home/HomeView.swift
@@ -7,14 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, World!")
+        Button(action: {
+                    
+        }) {
+            Text("Start").padding() 
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
---
+++ BabbelChallenge/Home/HomeViewRouter.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewRouter.swift
+//  BabbelChallenge
+//
+//  Created by Mamunul Mazid on 8/21/20.
+//
+
+import Foundation
+
+struct HomeViewRouter{
+    func routeToGameView(){
+        
+    }
+}
