b7250c436e5835753d536f17f93d1d59ffde0db8
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-21 15:25:34 +0600

    add dummy game view

---
+++ BabbelChallenge/GamePlay/GameView.swift
@@ -0,0 +1,26 @@
+//
+//  GameView.swift
+//  BabbelChallenge
+//
+//  Created by Mamunul Mazid on 8/21/20.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        VStack {
+            Text("abc")
+            Button(action: {}) {
+                Text("translation")
+            }
+            Text("Accuracy")
+        }
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
