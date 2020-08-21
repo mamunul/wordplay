7ec42dc659c33437980402e972f4cb8f5a58621d
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-21 17:52:49 +0600

    add animation completion modifier

---
+++ BabbelChallenge/GamePlay/AnimatableModifierDouble.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIView.swift
+//  BabbelChallenge
+//
+//  Created by Mamunul Mazid on 8/21/20.
+//
+
+import SwiftUI
+
+struct AnimatableModifierDouble: AnimatableModifier {
+    var targetValue: Double
+
+    var animatableData: Double {
+        didSet {
+            checkIfFinished()
+        }
+    }
+
+    var completion: () -> Void
+
+    init(bindedValue: Double, completion: @escaping () -> Void) {
+        self.completion = completion
+
+        animatableData = bindedValue
+        targetValue = bindedValue
+    }
+
+    func checkIfFinished() {
+        if animatableData == targetValue {
+            DispatchQueue.main.async {
+                self.completion()
+            }
+        }
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .animation(nil)
+    }
+}
