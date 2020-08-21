c9daacdbd0a4aa0c9813f4d8c7f5be5aa74346ed
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-21 15:28:42 +0600

    add game view router

---
+++ BabbelChallenge/GamePlay/GameViewRouter.swift
@@ -0,0 +1,15 @@
+//
+//  GameViewRouter.swift
+//  BabbelChallenge
+//
+//  Created by Mamunul Mazid on 8/21/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct GameViewRouter {
+    func route() -> some View {
+        GameView()
+    }
+}
