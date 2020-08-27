691326bf0a4b03083a0d9e4575e35e1ac34f0326
MamunulMazid <mamunul.mazid@gmail.com>
2020-08-27 12:16:19 +0600

    add error handling in TranslationRepository class

--- BabbelChallenge/GamePlay/Interactor/TranslationRepository.swift
+++ BabbelChallenge/GamePlay/Interactor/TranslationRepository.swift
@@ -13,30 +13,38 @@ struct Translation: Decodable {
 }
 
 protocol ITranslationRepository {
-    func getTranslation() -> [String: String]
+    func getTranslation() throws -> [String: String]
+}
+
+enum TranslationRepositoryError: Error {
+    case invalidURL, inValidJSON, inValidData
 }
 
 class TranslationRepository: ITranslationRepository {
     private var translationMap = [String: String]()
+    private let fileName = "words"
+    private let fielExtension = "json"
 
-    func getTranslation() -> [String: String] {
-        if translationMap.count > 0 {
+    func getTranslation() throws -> [String: String] {
+        if !translationMap.isEmpty {
             return translationMap
         }
 
-        let url = Bundle.main.url(forResource: "words", withExtension: "json")!
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: fielExtension) else {
+            throw TranslationRepositoryError.invalidURL
+        }
 
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
 
-        let jsonData = try? Data(contentsOf: url)
+        guard let jsonData = try? Data(contentsOf: url) else { throw TranslationRepositoryError.inValidData }
 
-        if jsonData != nil {
-            let result = try! decoder.decode([Translation].self, from: jsonData!)
+        guard let result = try? decoder.decode([Translation].self, from: jsonData) else {
+            throw TranslationRepositoryError.inValidJSON
+        }
 
-            for element in result {
-                translationMap[element.textEng] = element.textSpa
-            }
+        for element in result {
+            translationMap[element.textEng] = element.textSpa
         }
 
         return translationMap
